0ff2dbea31381f62e6dc17bb6d1fe9153d4bafa7
Hissen <hissenyt@naver.com>
2025-06-05 06:38:24 +0900

    feat: post 테이블 및 게시글 write 기능 구현 완료

--- db/init/init.sql
+++ db/init/init.sql
@@ -3,3 +3,10 @@ CREATE TABLE users(
     username varchar(20) NOT NULL,
     password varchar(20) NOT NULL
     ) ENGINE = innodb;
+
+CREATE TABLE posts(
+    title varchar(30) NOT NULL,
+    content varchar(100) NOT NULL,
+    writer varchar(20) NOT NULL,
+    crtime varchar(20) NOT NULL
+    ) ENGINE = innodb;
---
+++ src/write.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+date_default_timezone_set("Asia/Seoul");
+$con = mysqli_connect('db', 'exampleuser', 'examplepass', 'exampledb');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $username = $_SESSION['username'];
+    $crtime = date("Y-m-d H:i:s");
+
+    if ($con->connect_error){
+        die("DB connection error");
+    }
+
+    mysqli_query($con, "INSERT INTO posts (title, content, writer, crtime) VALUES ('$title', '$content', '$username','$crtime')");
+    header("Location: list.php");
+}
+?>
+
+<html>
+
+<body>
+
+<a href="index.php">home</a>
+<a href="write.php">write</a>
+<a href="list.php">list</a>
+<a href="logout.php">logout</a>
+
+<br/>
+<h1 style="font-family: 'Times New Roman', Times, serif;"> write </h1>
+<br/>
+
+<form method="post" action = "write.php">
+<input type="text" name="title" placeholder="Title"/>
+ <textarea name="content" placeholder="Content"></textarea>
+<input type="submit" value="Submit"/>
+<br/>
+
+</body>
+</html>
